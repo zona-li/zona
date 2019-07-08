1c975ac4762a0a4ef53138c5baef95c30ec10dca
ZonaClark <haoyang.zona@gmail.com>
2019-07-08 06:31:47 -0400

    add form

--- src/Component/Schedule/index.js
+++ src/Component/Schedule/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 
+import { useForm } from "./useForm";
+
 const Calendar = () => {
+    const [values, handleChange] = useForm({name: "", email: "", phone: ""});
+
     return (
-        <p>Replace me with calendar</p>
+        <div>
+            <p>Replace me with calendar</p>
+            <form>
+                Your name:
+                <input name="name" type="text" value={values.name} onChange={handleChange} />
+                <br></br>
+                Email:
+                <input name="email" type="email" value={values.email} onChange={handleChange} />
+                <br></br>
+                Phone number:
+                <input name="phone" type="tel" value={values.phone} onChange={handleChange} />
+            </form>
+        </div>
     );
 }
 
